6e25a815c26bbddd2c7373d8b2551f1d9bac4b7b
noah6570 <noah6570@gmail.com>
2023-02-08 00:43:52 +0900

    first commit

---
+++ EmotionDiary/ViewController/TabBar/TabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarController.swift
+//  EmotionDiary
+//
+//  Created by hoon Kim on 2023/02/07.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.selectedIndex = 1
+    print("I'm In !!!")
+  }
+  
+}
